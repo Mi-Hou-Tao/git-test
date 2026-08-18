1bad6b8328562d83adc21c6cb586caa096d173ed
blueberry <migliacciobowcutt@gmail.com>
2026-08-18 10:57:15 +0800

    Revert "by yx"
    This reverts commit 0ffc7d88924bc4f2fdb3d2e691d1bece834b16a8.

--- test2.0.c
+++ test2.0.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
+#include    <stdio.h>
 int main() {
-    char tmp[20];
-    printf("请输入你的名字: ");
-    scanf("%s", tmp);
-    printf("Hello, %s!\n", tmp);
+    printf("Hello, World!\n");
     return 0;
 }
-
-// 我来了
