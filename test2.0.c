0ffc7d88924bc4f2fdb3d2e691d1bece834b16a8
blueberry <migliacciobowcutt@gmail.com>
2026-08-18 10:52:32 +0800

    by yx

--- test2.0.c
+++ test2.0.c
@@ -1,5 +1,10 @@
-#include    <stdio.h>
+#include <stdio.h>
 int main() {
-    printf("Hello, World!\n");
+    char tmp[20];
+    printf("请输入你的名字: ");
+    scanf("%s", tmp);
+    printf("Hello, %s!\n", tmp);
     return 0;
 }
+
+// 我来了
