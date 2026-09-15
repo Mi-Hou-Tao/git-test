be206be0cefbb79ddc259661251debb9dedf6262
9336 <migliacciobowcutt@gmail.com>
2026-09-15 23:22:05 +0800

    handwritten calculator 0.1

--- 2.Sept.test/9.15.1.py
+++ 2.Sept.test/9.15.1.py
@@ -1,10 +1,26 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 import torch
-from torchvision import datasets,transforms
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
 
-transforms = transforms.ToTensor()
+transform = transforms.ToTensor()
+
 train_data = datasets.MNIST(
     root="./data",
     train=True,
     download=True,
     transform=transform
 )
+
+image,label = train_data[0]
+
+print("训练集数量：", len(train_data))
+print("图片形状：", image.shape)
+print("标签：", label)
+
+print("最大像素值：", image.max())
+print("最小像素值：", image.min())
+
+plt.imshow(image.squeeze(), cmap="gray", vmin=0, vmax=1)
+plt.show()
